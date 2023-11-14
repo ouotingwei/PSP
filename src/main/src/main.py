895b2c17c2f4d636471d88c76d00bcd4c5b224a5
OU, TING-WEI <87459096+ouotingwei@users.noreply.github.com>
2023-11-14 14:48:29 +0800

    Delete src directory

--- src/main/src/main.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import rospy
-from main_controller import main_controller
-import sys
-print(sys.path)
-
-if __name__ == "__main__":
-    mc = main_controller()
-    mc.run()
