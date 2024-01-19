9da75a1873c31b8e9631252575f7cb28cef19402
tingweiou <kklb1716@gmail.com>
2024-01-19 18:01:16 +0800

    1/19

--- src/vision_capture/src/vision_capture.py
+++ src/vision_capture/src/vision_capture.py
@@ -1,35 +1,37 @@
 import sys, os, math, time
 import matplotlib.pyplot as plt 
-from numpy import *
+import numpy as np
 from pupil_apriltags import Detector
 import cv2
 
 def apriltagLocalization(files,K):
     img = cv2.imread(files)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     plt.figure(figsize=(10, 10))
     plt.imshow(gray, cmap=plt.cm.gray)
 
     at_detector = Detector(families='tag36h11')
     tags = at_detector.detect(gray)
 
-    print("tags: {}".format(tags))
+    #print("tags: {}".format(tags))
 
-    for tag in tags:
-        print(tuple(tag.corners[0].astype(int)))
-        for i in range(4):
-            cv2.circle(img, tuple(tag.corners[i].astype(int)), 4, (255, 0, 0), 2)
+    # homography matrix
+    H = np.array(tags[0].homography.astype(float))
 
-        cv2.circle(img, tuple(tag.center.astype(int)), 4, (2, 180, 200), 4)
+    num, Rs, Ts, Ns = cv2.decomposeHomographyMat(H, K)
+
+    print("num: {}".format(num), "Rs: {}".format(Rs), "Ts: {}".format(Ts), "Ns: {}".format(Ns))
+
 
     plt.figure(figsize=(10,10))
     plt.imshow(img)
     plt.show()
 
+
 def main():
-    files = '/home/wei/PSP/files/test_2.jpg'
-    K = [623.6932761844039, 0.0, 329.01981432231025, 0.0, 624.1571561352914, 235.02669920374768, 0.0, 0.0, 1.0]
-    apriltagLocalization = os.path.join(files, K)
+    files = '/home/wei/PSP/files/test.jpg'
+    K = np.array([[623.6932761844039, 0.0, 329.01981432231025], [0.0, 624.1571561352914, 235.02669920374768], [0.0, 0.0, 1.0]])
+    apriltagLocalization(files, K)
 
 
 
