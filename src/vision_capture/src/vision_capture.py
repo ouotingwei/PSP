844f5cb2b99aae9b71682de4b386cecf1d6efa63
tingweiou <kklb1716@gmail.com>
2024-01-19 18:53:25 +0800

    not yet

--- src/vision_capture/src/vision_capture.py
+++ src/vision_capture/src/vision_capture.py
@@ -11,7 +11,7 @@ def apriltagLocalization(files,K):
     plt.imshow(gray, cmap=plt.cm.gray)
 
     at_detector = Detector(families='tag36h11')
-    tags = at_detector.detect(gray)
+    tags = at_detector.detect(gray,tag_size=0.015)
 
     #print("tags: {}".format(tags))
 
@@ -20,7 +20,26 @@ def apriltagLocalization(files,K):
 
     num, Rs, Ts, Ns = cv2.decomposeHomographyMat(H, K)
 
-    print("num: {}".format(num), "Rs: {}".format(Rs), "Ts: {}".format(Ts), "Ns: {}".format(Ns))
+    # nums = possible solutions will be returned
+    # Rs = contains a list of the rotation matrix
+    # Ts = contains a list of the translation matrix
+    # Ns = contains a list of the normal vector of the plane
+    #print("num: {}".format(num), "Rs: {}".format(Rs), "Ts: {}".format(Ts), "Ns: {}".format(Ns))
+    for i in range(num):
+        if Ns[i][2] < 0:
+            correct_solution_index = i
+            break
+
+    correct_R = Rs[correct_solution_index]
+    correct_T = Ts[correct_solution_index]
+    correct_N = Ns[correct_solution_index]
+
+    print("Correct Rotation Matrix:")
+    print(correct_R)
+    print("Correct Translation Vector:")
+    print(correct_T)
+    print("Correct Normal Vector:")
+    print(correct_N)
 
 
     plt.figure(figsize=(10,10))
@@ -29,7 +48,7 @@ def apriltagLocalization(files,K):
 
 
 def main():
-    files = '/home/wei/PSP/files/test.jpg'
+    files = '/home/wei/PSP/files/14.jpg'
     K = np.array([[623.6932761844039, 0.0, 329.01981432231025], [0.0, 624.1571561352914, 235.02669920374768], [0.0, 0.0, 1.0]])
     apriltagLocalization(files, K)
 
